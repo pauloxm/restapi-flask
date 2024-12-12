334d1ddfc465abf0b16d1d2e9a41ff5d83028a09
Paulo Xavier <pauloxmachado@gmail.com>
2024-12-12 18:06:57 -0300

    feat: Add SQLAlchemy

--- app.py
+++ app.py
@@ -1,49 +1,44 @@
-from flask import Flask, request, jsonify
-from flask import request
-# Importa as bibliotecas Resource e API
-from flask_restful import Resource, Api
-import mariadb
-import json
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb://root:admin@mariadb/flask'
+db = SQLAlchemy(app)
+
+
+# Classe referente a tabela do banco
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(50), nullable=False)
+    completed = db.Column(db.Integer, nullable=False)
+
+    # Metodo de representação
+
+    def __repr__(self):
+        return f"Tasks('{self.description}','{self.completed}')"
+
+    # Monta o dicionario para serialização
+    def as_dict(self):
+        return {c.description: getattr(self, c.description) for c in self.__table__.columns}
+
+
 # Comando abaixo cria API com flask Restful
 api = Api(app)
 
-# Parametros de conexão com o banco de dados
-config = {
-    'host': 'mariadb',
-    'port': 3306,
-    'user': 'root',
-    'password': 'admin',
-    'database': 'flask'
-}
 
-@app.route('/api/people', methods=['GET','POST'])
+@app.route('/api/tasks', methods=['GET', 'POST'])
 def index():
-   # Conecta com o MariaDB
-   conn = mariadb.connect(**config)
-   # Cria o cursor de conexão
-   cur = conn.cursor()
 
-   json_data = []
+    if request.method == 'GET':
+        tasks = Tasks.query.all()
+        return jsonify([task.as_dict() for task in tasks])
 
-   if request.method == 'GET':
-    sql = "SELECT * FROM tasks ORDER BY id DESC"
-    cur.execute(sql)
-    row_headers=[x[0] for x in cur.description]
-    for result in cur:
-        json_data.append(dict(zip(row_headers,result)))
+    if request.method == 'POST':
+        tasks = Tasks.query.all()
+        return jsonify([task.as_dict() for task in tasks])
 
-   if request.method == 'POST':
-       data = request.get_json()
-       tarefa = (data['description'], data['completed'])
-       sql = "INSERT INTO tasks (description, completed) VALUES (%s, %s)"
-       cur.execute(sql, tarefa)
-       conn.commit()
-       json_data = { 'success': True }
-
-   # Retorna JSON com a saida
-   return json.dumps(json_data)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="5000")
