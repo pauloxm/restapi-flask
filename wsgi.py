d8447e1a96c6c5c9e3486745b3b86c3adaa9354b
Paulo Xavier <pauloxmachado@gmail.com>
2024-12-28 03:03:42 -0300

    fix: force DevConfig

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,8 @@ import os
 if os.getenv("FLASK_ENV") == "development":
     app = create_app('config.DevConfig')
 else:
-    app = create_app('config.MockConfig')
+    # app = create_app('config.MockConfig')
+    app = create_app('config.DevConfig')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="5000")
