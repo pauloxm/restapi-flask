e42554e3684193ad1331f22a927a50447d997834
Paulo Xavier <pauloxmachado@gmail.com>
2024-12-28 22:31:56 -0300

    feat: Add Table users

--- dump.sql
+++ dump.sql
@@ -4,6 +4,23 @@ CREATE DATABASE IF NOT EXISTS flask;
 
 USE flask;
 
+-- Table structure for table `users`
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `login` varchar(30) UNIQUE DEFAULT NOT NULL,
+    `password` varchar(30) DEFAULT NOT NULL,
+    `name` varchar(100) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ;
+
+-- Dumping data for table `users`
+
+INSERT INTO `users` (
+    `login`,`password`,`name`)
+    values
+    ('john','$2a$10$6hEqi3ZvVJRbVVaaxOC30eH2vaNTTCK/nD569KAILMqUBRT7KTW32','John Doe');
+-- Password bcrypt 10x - john.doe
 -- Table structure for table `tasks`
 
 CREATE TABLE IF NOT EXISTS `tasks` (
@@ -12,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `tasks` (
     `completed` int DEFAULT NULL,
     `ticket_id` varchar(11) UNIQUE DEFAULT NULL,
     PRIMARY KEY (`id`)
-    ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ;
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ;
 
 -- Dumping data for table `tasks`
 
@@ -27,4 +44,4 @@ CREATE TABLE IF NOT EXISTS `healthcheck` (
     `id` bigint NOT NULL AUTO_INCREMENT,
     `status` TINYINT(1) DEFAULT NULL,
     PRIMARY KEY (`id`)
-    ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ;
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ;
