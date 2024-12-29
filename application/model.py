50160439a94cfb6fe242a4aff0d0374fb8aa0879
Paulo Xavier <pauloxmachado@gmail.com>
2024-12-28 22:41:35 -0300

    feat: Add Class Users

--- application/model.py
+++ application/model.py
@@ -2,6 +2,7 @@ from .db import db
 
 
 class Tasks(db.Model):
+    __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(50), nullable=False)
     completed = db.Column(db.Integer, nullable=False)
@@ -22,6 +23,7 @@ class Tasks(db.Model):
 
 
 class Healthcheck(db.Model):
+    __tablename__ = 'healthcheck'
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String(1), nullable=False)
 
@@ -32,4 +34,25 @@ class Healthcheck(db.Model):
     # Monta o dicionario para serialização
     def as_dict(self):
         return {c.status: getattr(self, c.status)
+                for c in self.__table__.columns}
+
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    name = db.Column(db.String(100), nullable=True)
+
+    # Metodo de representação
+    def __repr__(self):
+        return (
+            "Users('{self.login}',"
+            "'{self.password}',"
+            "'{self.name}')"
+        )
+
+    # Monta o dicionario para serialização
+    def as_dict(self):
+        return {c.login: getattr(self, c.login)
                 for c in self.__table__.columns}
