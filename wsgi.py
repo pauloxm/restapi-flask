d3afd304f30452e66eb46a3349512c16490f03ad
Paulo Xavier <pauloxmachado@gmail.com>
2024-12-29 15:23:01 -0300

    feat: Add condition to run in debug mode only in DevConfig

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,9 @@ import os
 
 if os.getenv("FLASK_ENV") == "development":
     app = create_app('config.DevConfig')
+    if __name__ == '__main__':
+        app.run(debug=True, host="0.0.0.0", port="5000")
 else:
-    # app = create_app('config.MockConfig')
-    app = create_app('config.DevConfig')
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port="5000")
+    app = create_app('config.ProdConfig')
+    if __name__ == '__main__':
+        app.run(host="0.0.0.0", port="5000")
